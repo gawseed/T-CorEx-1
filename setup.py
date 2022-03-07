550c48257949e224ddface793b9f2d2f35ea8754
Wes Hardaker <opensource@hardakers.net>
2022-03-07 14:28:32 -0800

    fix licensing list

--- setup.py
+++ setup.py
@@ -28,12 +28,12 @@ setup(name='gawseed-tcorex',
           'Topic :: Scientific/Engineering :: Artificial Intelligence',
           'Topic :: Scientific/Engineering :: Information Analysis',
           'Topic :: Scientific/Engineering :: Mathematics',
-          'Programming Language :: Python :: 3'
+          'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
           'Programming Language :: Python :: 3.9',
           'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: GNU Affero General Public License v3'
+          'License :: OSI Approved :: GNU Affero General Public License v3',
       ],
       packages=['tcorex', 'tcorex.experiments', 'tcorex.tools'])
